7498f8cfa1ae9561aa0b7ef362c4daa63323b05b
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-02-23 09:38:22 +0100

    [add] bg-color to fizzbuzz

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ console.log('js-fizzbuzz');
 const box = document.getElementById('container-box');
 for(let i = 1; i <= 100; i++){
   if(!(i % 3) && !(i % 5)){
-    box.innerHTML += `<div class="box yellow green">fizzbuzz</div>`
+    box.innerHTML += `<div class="box red">fizzbuzz</div>`
   }else if (!(i % 3)){
     box.innerHTML += `<div class="box yellow">fizz</div>`
   }else if (!(i % 5)){
